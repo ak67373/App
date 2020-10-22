a7bc1d9f3851286d3c9a775dfdd816728d2071b8
ak67373 <39940399+ak67373@users.noreply.github.com>
2020-10-22 20:44:07 +0530

    Delete apiurl.py

--- apiurl.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import register_agent, authenticate_agent, get_todo_items, save_todo_items
-
-urlpatterns = [
-    url(r'^app/agent', register_agent),
-    url(r'^app/auth', authenticate_agent),
-    url(r'^app/sites/list/?agent={agentId}', get_todo_items),
-    url(r'^app/sites?agent={agentId}', save_todo_items),
-]
