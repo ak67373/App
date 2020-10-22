91219019c1afa8bb1cf3f92d131875e4c9d1af8b
ak67373 <39940399+ak67373@users.noreply.github.com>
2020-10-22 20:44:30 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-
-def encrypt(hash_str):
-    key = "secret-key-ajsfj"
-    hash_str = hash_str + "|" + key
-    h = hashlib.sha512()
-    h.update(hash_str)
-    return h.hexdigest()
